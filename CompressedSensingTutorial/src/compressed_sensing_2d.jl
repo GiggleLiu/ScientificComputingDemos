bb7ae775faaa259385e35314f692b1c95b30e015
GiggleLiu <cacate0129@gmail.com>
2024-03-20 02:29:27 +0800

    fix ci

--- CompressedSensingTutorial/src/compressed_sensing_2d.jl
+++ CompressedSensingTutorial/src/compressed_sensing_2d.jl
@@ -40,21 +40,21 @@ end
 function gradient_dct!(g, x, samples::ImageSamples; C=0.0)
     # gradient is 2 * (A' * A x - A' * sample_values) + sign(x)
     Ax_b = FFTW.idct(x)[samples.indices] .- samples.values
-    padded = zero_padded(samples, Ax_b)
+    padded = zero_padded(samples.size, samples.indices, Ax_b)
     if !iszero(C)
-        g .= 2 .* FFTW.dct(padded) .+ C .* sign.(x)
+        g .= 2 .* FFTW.dct!(padded) .+ C .* sign.(x)
     else
-        g .= 2 .* FFTW.dct(padded)
+        g .= 2 .* FFTW.dct!(padded)
     end
     return g
 end
 gradient_dct(x, samples::ImageSamples; C=0.0) = gradient_dct!(zero(x), x, samples; C)
 
 # zero pad the sample to target size
-function zero_padded(samples::ImageSamples{T}, values::AbstractVector{T}=samples.values) where T
-    x = similar(values, samples.size...)
+function zero_padded(size, indices, values::AbstractVector{T}) where T
+    x = similar(values, size...)
     fill!(x, zero(T))
-    x[samples.indices] .= values
+    x[indices] .= values
     return x
 end
 
