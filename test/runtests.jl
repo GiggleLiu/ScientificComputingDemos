e87616f9c4ffeab12d42245a3c721375eeaee972
YidaiZhang <yzhang958@connect.hkust-gz.edu.cn>
2024-03-19 19:46:53 +0800

    up

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,31 @@
 using SimpleLinearAlgebra
 using Test
 
-@testset "SimpleLinearAlgebra.jl" begin
+@testset "back_substitution.jl" begin
     include("back_substitution.jl")
+end
+
+@testset "lu_factorization.jl" begin
     include("lu_factorization.jl")
+end
+
+@testset "lu_factorization_partialpivoting.jl" begin
     include("lu_factorization_partialpivoting.jl")
+end
+
+@testset "householder.jl" begin
     include("householder.jl")
+end
+
+@testset "qr_factorization.jl" begin
     include("qr_factorization.jl")
+end
+
+@testset "orthogonalization.jl" begin
     include("orthogonalization.jl")
+end
+
+@testset "fouriertransform.jl" begin
     include("fouriertransform.jl")
 end
 
