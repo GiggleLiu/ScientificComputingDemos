8ebe9438767866fc34c1c7138218559077bd44d4
GiggleLiu <cacate0129@gmail.com>
2025-03-23 00:11:56 +0800

    update

--- SpinDynamics/src/SpinDynamics.jl
+++ SpinDynamics/src/SpinDynamics.jl
@@ -6,7 +6,7 @@ using LinearAlgebra, StaticArrays, Graphs
 export simulate!, ClassicalSpinSystem, random_spins, TrotterSuzuki, TimeDependent, energy
 export visualize_spins_animation, visualize_spins
 
-include("spindynamics.jl")
+include("simulation.jl")
 include("visualize.jl")
 
 end
